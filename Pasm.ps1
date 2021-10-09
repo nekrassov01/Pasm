61b0ef401beda38902f8ce3a69294da1cb67d221
nekrassov01 <unclek62@gmail.com>
2021-10-09 23:47:03 +0900

    Fix 'Pasm.ps1'

--- Pasm.ps1
+++ Pasm.ps1
@@ -29,27 +29,27 @@ end {
     }
 
     # Variables
-    $module     = [path]::GetFileNameWithoutExtension($PSCommandPath)
-    $moduleDir  = $PSScriptRoot, 'src' -join $sepalator
-    $modulePath = $moduleDir, $('{0}.psd1' -f $module) -join $sepalator
-    $releaseDir = $PSScriptRoot, 'release', $module -join $sepalator
-    $author     = 'nekrassov01'
+    $moduleName   = [path]::GetFileNameWithoutExtension($PSCommandPath)
+    $moduleDir    = $PSScriptRoot, 'src' -join $sepalator
+    $manifestPath = $moduleDir, $('{0}.psd1' -f $moduleName) -join $sepalator
+    $releaseDir   = $PSScriptRoot, 'release', $moduleName -join $sepalator
+    $author       = 'nekrassov01'
 
     # Manifest parameters
     $script:moduleManifest = @{
         Guid                       = [guid]::NewGuid().Guid
-        Path                       = $modulePath
+        Path                       = $manifestPath
         Author                     = $author
         CompanyName                = 'Unknown'
         Copyright                  = '(c) {0} All rights reserved.' -f $author
-        RootModule                 = '{0}.psm1' -f $module
+        RootModule                 = '{0}.psm1' -f $moduleName
         ModuleVersion              = $version
-        Description                = '{0} is a PowerShell module for simple management of public IP address ranges provided by AWS.' -f $module
+        Description                = '{0} is a PowerShell module for simple management of public IP address ranges provided by AWS.' -f $moduleName
         PowerShellVersion          = '5.1'
         DotNetFrameworkVersion     = '4.5'
         ClrVersion                 = '4.0.0.0'
         CompatiblePSEditions       = @('Core', 'Desktop')
-       #RequiredModules            = @()
+        RequiredModules            = @('PowerShell-Yaml', 'AWS.Tools.Common', 'AWS.Tools.EC2')
        #RequiredAssemblies         = @()
         ExternalModuleDependencies = @('PowerShell-Yaml', 'AWS.Tools.Common', 'AWS.Tools.EC2')
         CmdletsToExport            = @()
@@ -57,15 +57,15 @@ end {
         VariablesToExport          = '*'
         AliasesToExport            = @()
         Tags                       = @('AWS')
-        ProjectUri                 = 'https://github.com/{0}/{1}' -f $author, $module
-        LicenseUri                 = 'https://github.com/{0}/{1}/blob/main/LICENSE' -f $author, $module
-        ReleaseNotes               = 'https://github.com{0}/{1}/releases/tag/{2}' -f $author, $module, $version
+        ProjectUri                 = 'https://github.com/{0}/{1}' -f $author, $moduleName
+        LicenseUri                 = 'https://github.com/{0}/{1}/blob/main/LICENSE' -f $author, $moduleName
+        ReleaseNotes               = 'https://github.com{0}/{1}/releases/tag/{2}' -f $author, $moduleName, $version
        #DefaultCommandPrefix       = $module
     }
 
     try {
         # Create or update manifest
-        if (Test-Path -LiteralPath $modulePath) {
+        if (Test-Path -LiteralPath $manifestPath) {
             $moduleManifest.Remove('Guid')
             Update-ModuleManifest @moduleManifest
         }
@@ -73,7 +73,7 @@ end {
             New-ModuleManifest @moduleManifest
         }
         # Validate manifest
-        Test-ModuleManifest -Path $modulePath
+        Test-ModuleManifest -Path $manifestPath
     }
     catch {
         $PSCmdlet.ThrowTerminatingError($PSItem)
@@ -92,6 +92,8 @@ end {
         if ([string]::IsNullOrWhiteSpace($nuGetApiKey)) {
             throw [InvalidOperationException]::new('The nuget api key is empty. Please specify it.')
         }
+        Import-Module $manifestPath -PassThru -Verbose -Force
+        Get-Module -Name $moduleName
         Publish-Module -Path $releaseDir -NuGetApiKey $nuGetApiKey
     }
 }
